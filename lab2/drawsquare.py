19e0f75394101b05e90b01d2189e2c5092b14f4d
Keng <chan1251@umn.edu>
2017-09-14 17:52:23 -0500

    Added square drawing

---
+++ lab2/drawsquare.py
@@ -0,0 +1,11 @@
+import turtle
+def drawsquare():
+    turtle.forward(sidelength)
+    turtle.right(90)
+    turtle.forward(sidelength)
+    turtle.right(90)
+    turtle.forward(sidelength)
+    turtle.right(90)
+    turtle.forward(sidelength)
+sidelength = 300
+drawsquare()
